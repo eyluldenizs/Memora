c89434a499faeef46fa7fd8235ad6b6bc840ae35
eyluldenizs <soylueyluldeniz@gmail.com>
2026-01-22 13:54:24 +0300

    Initial Commit

---
+++ Memora/MemoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoraApp.swift
+//  Memora
+//
+//  Created by Eylül Soylu on 22.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct MemoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MemoraTests/MemoraTests.swift
@@ -0,0 +1,17 @@
+//
+//  MemoraTests.swift
+//  MemoraTests
+//
+//  Created by Eylül Soylu on 22.01.2026.
+//
+
+import Testing
+@testable import Memora
+
+struct MemoraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
